36e9f31be37308708aff28370446149d0bf2cf60
zhangxinyue <Xinyue.Zhang@ibm.com>
2019-09-04 10:54:48 +0800

    add invoke template

--- chaincode/simple/installChaincode.sh
+++ chaincode/simple/installChaincode.sh
@@ -8,3 +8,7 @@ docker exec -e "CORE_PEER_ADDRESS=peer1.org2.example.com:7051" -e "CORE_PEER_LOC
 
 # TODO: not yet success
 docker exec -e "CORE_PEER_ADDRESS=peer0.org1.example.com:7051" -e "CORE_PEER_LOCALMSPID=Org1" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/org1.example.com/users/Admin@org1.example.com/msp" chaincode.helper.example.com peer chaincode instantiate -o orderer.ordererorg.example.com:7050 -C mychannel -n simple -l "$CC_RUNTIME_LANGUAGE" -v 1.0 -c '{"Args":[]}' -P "AND ('Org1.member','Org2.member')"
+
+# Invoke example
+# peer chaincode invoke -o orderer.ordererorg.example.com:7050 -C mychannel -n simple -c '{"function": "Ping", "Args":[]}'
+# peer chaincode invoke -o orderer.ordererorg.example.com:7050 -C mychannel -n simple -c '{"function": "Create", "Args":["test"]}'
