803402511151c885b2a8129d6e3b2d4189419b12
zhangxinyue <Xinyue.Zhang@ibm.com>
2019-12-09 10:39:51 +0800

    remove useless

--- chaincode/token/service/token-service.go
+++ chaincode/token/service/token-service.go
@@ -4,12 +4,10 @@ import (
 	"github.com/chaincode_learning_go/chaincode/token/repo"
 	"github.com/chaincode_learning_go/chaincode/token/service/impl"
 	"github.com/chaincode_learning_go/chaincode/token/service/iterf"
-	"sync"
 )
 
 var TokenService iterf.ITokenService
 var TokenLogService iterf.ITokenLogService
-var once sync.Once
 
 func init() {
 	TokenService = &impl.TokenService{
